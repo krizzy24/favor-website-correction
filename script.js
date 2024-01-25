29a11ef188f1e40d4bebbc793bfdffda6dee3464
krizzy24 <146850626+krizzy24@users.noreply.github.com>
2024-01-25 15:25:11 +0100

    second

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+  function submitForm() {
+    // You can add additional form validation logic here before submitting
+    // For simplicity, we'll just show a success message
+
+    // Simulating a successful form submission
+    alert("Form successfully submitted!");
+  }
